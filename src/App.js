949a57960f25cbcc7e31bd1c68fea8be87a5ae59
ArdoHintoso <awilliams.hintoso@gmail.com>
2023-10-31 22:40:05 -0700

    [v0.2.1] begin react router integration

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import "./App.css";
 import Header from "./Header";
 import MathExerciseContainer from "./components/MathExerciseContainer";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <ChakraProvider>
-      <div className="App">
-        <Header className="App-header" />
-        <MathExerciseContainer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Header className="App-header" />
+        </div>
+        <Routes>
+          <Route path="/worksheet" element={<MathExerciseContainer />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
