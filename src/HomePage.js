3642655d3ad1f7f9d3e76fc278b02e56f1a1e1f4
ArdoHintoso <awilliams.hintoso@gmail.com>
2023-11-02 20:35:34 -0700

    [v0.2.1] began table UI for user's past worksheets

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,13 +1,60 @@
-import { Center, Text } from "@chakra-ui/react";
+import {
+  Center,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  const date = new Date();
+
   return (
-    <Center>
-      <Link to="/worksheet">
-        <Text>Begin Worksheet</Text>
-      </Link>
-    </Center>
+    <div>
+      <Center>
+        <Link to="/worksheet">
+          <Text>Start New Worksheet</Text>
+        </Link>
+      </Center>
+
+      <TableContainer color="teal">
+        <Table variant="simple" colorScheme="teal">
+          <TableCaption> __________&apos;s Past Worksheets</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Date</Th>
+              <Th>Status</Th>
+              <Th isNumeric>Accuracy</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>{date.toString()}</Td>
+              <Td>Complete</Td>
+              <Td isNumeric>100.0%</Td>
+            </Tr>
+            <Tr>
+              <Td>XXX</Td>
+              <Td>Incomplete</Td>
+              <Td isNumeric>5.0%</Td>
+            </Tr>
+            <Tr>
+              <Td>XXX</Td>
+              <Td>Incomplete</Td>
+              <Td isNumeric>0.0%</Td>
+            </Tr>
+          </Tbody>
+          <Tfoot></Tfoot>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
